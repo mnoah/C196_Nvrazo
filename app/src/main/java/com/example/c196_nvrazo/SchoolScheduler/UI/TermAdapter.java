db4a7355309692a24b1f53b273fdfd47112dc50b
mnoah <noahvrazo@gmail.com>
2022-12-18 19:51:17 -0500

    Database stuff, Main menu option

--- app/src/main/java/com/example/c196_nvrazo/SchoolScheduler/UI/TermAdapter.java
+++ app/src/main/java/com/example/c196_nvrazo/SchoolScheduler/UI/TermAdapter.java
@@ -2,6 +2,7 @@ package com.example.c196_nvrazo.SchoolScheduler.UI;
 
 import android.content.Context;
 import android.content.Intent;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -23,40 +24,61 @@ public class TermAdapter extends RecyclerView.Adapter<TermAdapter.TermViewHolder
         private TermViewHolder(View itemview) {
             super(itemview);
             termItemView = itemview.findViewById(R.id.textView2);
-            itemview.setOnClickListener(new View.OnClickListener(){
+            itemview.setOnClickListener(new View.OnClickListener() {
                 @Override
-                public void onClick (View view){
-                    int position=getAdapterPosition();
-                    final Term current=mTerms.get(position);
-                    Intent intent= new Intent(context, TermAdapter.class)
-
+                public void onClick(View view) {
+                    int position = getAdapterPosition();
+                    final Term current = mTerms.get(position);
+                    Intent intent = new Intent(context, TermAdapter.class);
+                    intent.putExtra("TermID", current.getTermID());
+                    intent.putExtra("TermName", current.getTermName());
+                    intent.putExtra("TermStartDate", current.getTermStartDate());
+                    intent.putExtra("TermEndDate", current.getTermEndDate());
                 }
 
             });
         }
 
     }
+
     private List<Term> mTerms;
-    private final Context  contex;
+    private final Context context;
+    private final LayoutInflater mInflater;
+
+    public TermAdapter(Context context) {
+        mInflater = LayoutInflater.from(context);
+        this.context = context;
+        ;
+    }
 
     @NonNull
     @Override
     public TermAdapter.TermViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View itemView = mInflater.inflate(R.layout.term_list_item, parent, false);
+        return new TermViewHolder((itemView));
     }
 
     @Override
     public void onBindViewHolder(@NonNull TermAdapter.TermViewHolder holder, int position) {
+        if (mTerms != null) {
+            Term current = mTerms.get(position);
+            String name = current.getTermName();
+            holder.termItemView.setText(name);
+
+        } else {
+            holder.termItemView.setText("No Terms Found");
+        }
 
     }
 
     @Override
     public int getItemCount() {
+
         return mTerms.size();
     }
 
-    public void setTerms(List<Term> term){
-        mTerms=term;
+    public void setTerms(List<Term> term) {
+        mTerms = term;
         notifyDataSetChanged();
 
     }
--- app/src/main/java/com/example/c196_nvrazo/SchoolScheduler/UI/TermList.java
+++ app/src/main/java/com/example/c196_nvrazo/SchoolScheduler/UI/TermList.java
@@ -1,6 +1,7 @@
 package com.example.c196_nvrazo.SchoolScheduler.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
@@ -8,15 +9,22 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.example.c196_nvrazo.R;
+import com.example.c196_nvrazo.SchoolScheduler.Database.Repository;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class TermList extends AppCompatActivity {
+    private Repository repository;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_term_list);
         RecyclerView recyclerView=findViewById(R.id.TermListRecyclerView);
+        final TermAdapter termAdapter = new TermAdapter( this);
+        recyclerView.setAdapter(termAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        repository= new Repository()
+
         FloatingActionButton button = findViewById(R.id.TermListAddButton);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
